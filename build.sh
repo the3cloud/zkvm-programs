dbbd456e795540700c9ece7b9c450ef6558a37bf
steve-the3cloud <steve@the3.cloud>
2025-03-13 00:38:42 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 # TODO: Add check for `cargo` `cargo prove`, `cargo risczero` and `docker`
 
-RISC0_TLS_ELF_PATH=./target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/zktls_r0/zktls-r0
-SP1_TLS_ELF_PATH=./target/elf-compilation/docker/riscv32im-succinct-zkvm-elf/release/zktls-sp1
+RISC0_TLS_ELF_PATH=target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/zktls_r0/zktls-r0
+SP1_TLS_ELF_PATH=zktls/sp1/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/zktls-sp1
 
 TARGET_ELF_OUTPUTS_PATH=./target/elf-outputs
 SP1_TLS_ELF_FILENAME=zktls-sp1
@@ -13,13 +13,11 @@ cargo build --release || exit 1
 
 echo "Building zktls-sp1"
 cd zktls/sp1 || exit 1
-cargo prove build --docker --binary zktls-sp1 || exit 1
+cargo prove build --docker --workspace-directory ../../ || exit 1
+cd ../../ || exit 1
 
 echo "Building zktls-r0"
-cd ../../ || exit 1
 cargo risczero build --manifest-path zktls/r0/Cargo.toml || exit 1
-
-cargo prove vkey --elf $SP1_TLS_ELF_PATH || exit 1
 
 echo "Creating elf outputs"
 
@@ -27,6 +25,8 @@ mkdir -p $TARGET_ELF_OUTPUTS_PATH || exit 1
 
 cp $SP1_TLS_ELF_PATH $TARGET_ELF_OUTPUTS_PATH/$SP1_TLS_ELF_FILENAME || exit 1
 cp $RISC0_TLS_ELF_PATH $TARGET_ELF_OUTPUTS_PATH/$R0_TLS_ELF_FILENAME || exit 1
+
+echo "Generating image id and vkey"
 
 target/release/compute-imageid $TARGET_ELF_OUTPUTS_PATH/$R0_TLS_ELF_FILENAME > $TARGET_ELF_OUTPUTS_PATH/$R0_TLS_ELF_FILENAME.key || exit 1
 target/release/compute-vkey $TARGET_ELF_OUTPUTS_PATH/$SP1_TLS_ELF_FILENAME > $TARGET_ELF_OUTPUTS_PATH/$SP1_TLS_ELF_FILENAME.vkey || exit 1
