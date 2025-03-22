2f70a3e8418ceba1159d8c97d68234bc06a30550
steve-the3cloud <steve@the3.cloud>
2025-03-23 00:24:52 +0800

    add precompiles for risc0

--- zktls/r0/src/main.rs
+++ zktls/r0/src/main.rs
@@ -1,16 +1,11 @@
-use zktls_program_core::GuestInput;
-
 use std::io::Read;
 
 fn main() {
-    println!("Hello, world!");
     let mut input = Vec::new();
 
-    let length = risc0_zkvm::guest::env::stdin()
+    risc0_zkvm::guest::env::stdin()
         .read_to_end(&mut input)
         .unwrap();
-
-    println!("length: {}", length);
 
     let output = zktls_replayable_tls::entry(&input);
 
