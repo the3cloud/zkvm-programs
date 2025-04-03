83c17155d224db3db845b7d0e11aa3e3878046ec
steve-the3cloud <steve@the3.cloud>
2025-04-03 08:12:40 +0800

    add wasm impl

--- wasm/zktls/src/lib.rs
+++ wasm/zktls/src/lib.rs
@@ -3,11 +3,39 @@ mod utils;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub struct Request {
+    request: zktls_program_core::Request,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, zktls!");
+impl Request {
+    pub fn from_value(value: JsValue) -> Result<Self, JsValue> {
+        let request = serde_wasm_bindgen::from_value(value)?;
+        Ok(Self { request })
+    }
+
+    pub fn to_value(&self) -> Result<JsValue, JsValue> {
+        serde_wasm_bindgen::to_value(&self.request).map_err(|e| JsValue::from(e.to_string()))
+    }
+
+    pub fn request_hash(&self) -> Result<Box<[u8]>, JsValue> {
+        let hash = self.request.request_hash();
+        Ok(hash.to_vec().into_boxed_slice())
+    }
+
+    pub fn request_id(&self) -> Result<Box<[u8]>, JsValue> {
+        let id = self
+            .request
+            .request_id()
+            .map_err(|e| JsValue::from(e.to_string()))?;
+        Ok(id.to_vec().into_boxed_slice())
+    }
+
+    pub fn dapp(&self) -> Result<Box<[u8]>, JsValue> {
+        let dapp = self
+            .request
+            .dapp()
+            .map_err(|e| JsValue::from(e.to_string()))?;
+        Ok(dapp.to_vec().into_boxed_slice())
+    }
 }
